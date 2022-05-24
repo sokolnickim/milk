64802a1ce304c6cb80e499d160246bc5b79c5acc
Michael Sokolnicki <msokolnicki@me.com>
2022-05-23 19:40:14 -0700

    Additional sleep validations and unit tests

--- lib/milk/sleep/session.ex
+++ lib/milk/sleep/session.ex
@@ -1,5 +1,6 @@
 defmodule Milk.Sleep.Session do
   use Ecto.Schema
+  require Ecto.Query
   import Ecto.Changeset
 
   schema "sleep_sessions" do
@@ -13,28 +14,61 @@ defmodule Milk.Sleep.Session do
     session
     |> cast(attrs, [:started_at, :ended_at, :note])
     |> validate_required([:started_at, :ended_at])
+    |> validate_change(:started_at, &is_in_the_past/2)
+    |> validate_change(:ended_at, &is_in_the_past/2)
     |> validate_duration()
+    |> validate_overlap()
+  end
+
+  def is_in_the_past(field, time) do
+    if time > NaiveDateTime.local_now(),
+      do: [{field, "must be in the past"}],
+      else: []
   end
 
   def validate_duration(changeset) do
+    with {:ok, started_at} <- fetch_change(changeset, :started_at),
+         {:ok, ended_at} <- fetch_change(changeset, :ended_at) do
+      duration = NaiveDateTime.diff(ended_at, started_at)
+
+      cond do
+        duration < 0 -> add_error(changeset, :duration, "end must be after start")
+        duration < 300 -> add_error(changeset, :duration, "must be at least 5 minutes")
+        true -> changeset
+      end
+    else
+      _ -> changeset
+    end
+  end
+
+  def validate_overlap(changeset) do
     started_at = get_change(changeset, :started_at)
     ended_at = get_change(changeset, :ended_at)
 
     changeset
-    |> validate_change(:started_at, &duration_validator(&1, &2, ended_at))
-    |> validate_change(:ended_at, &duration_validator(&1, started_at, &2))
+    |> validate_change(:started_at, &overlap_validator(&1, &2, ended_at))
+    |> validate_change(:ended_at, &overlap_validator(&1, started_at, &2))
   end
 
-  defp duration_validator(field, started_at, ended_at) do
-    duration = NaiveDateTime.diff(ended_at, started_at)
-
-    cond do
-      duration < 0 -> [{field, negative_duration_error(field)}]
-      duration < 300 -> [{field, "sleep session must be at least 5 minutes"}]
-      true -> []
+  defp overlap_validator(field, started_at, ended_at) do
+    field
+    |> overlap_query(started_at, ended_at)
+    |> Milk.Repo.all()
+    |> case do
+      [] -> []
+      _ -> [{field, "overlaps with existing session"}]
     end
   end
 
-  defp negative_duration_error(:started_at), do: "must be before ending time"
-  defp negative_duration_error(:ended_at), do: "must be after starting time"
+  defp overlap_query(:started_at, started_at, ended_at) do
+    __MODULE__
+    |> Ecto.Query.where([s], s.ended_at >= ^started_at)
+    |> Ecto.Query.where([s], s.ended_at <= ^ended_at)
+  end
+
+  defp overlap_query(:ended_at, started_at, ended_at) do
+    __MODULE__
+    |> Ecto.Query.where([s], s.started_at >= ^started_at)
+    |> Ecto.Query.where([s], s.started_at <= ^ended_at)
+  end
 end
---
+++ test/milk/sleep_test.exs
@@ -0,0 +1,73 @@
+defmodule Milk.SleepTest do
+  use Milk.DataCase
+
+  alias Milk.Sleep
+
+  describe "sleep" do
+    def sleep_fixtures() do
+      attrs = [
+        %{started_at: ~N[2022-05-16 18:30:00], ended_at: ~N[2022-05-17 06:00:00]},
+        %{started_at: ~N[2022-05-17 09:00:00], ended_at: ~N[2022-05-17 10:30:00]},
+        %{started_at: ~N[2022-05-17 13:00:00], ended_at: ~N[2022-05-17 15:00:00]}
+      ]
+
+      for attr <- attrs do
+        {:ok, sleep} = Sleep.create_session(attr)
+        sleep
+      end
+    end
+
+    test "list_day_sessions/0 returns all sessions" do
+      [_sleep1, sleep2, sleep3] = sleep_fixtures()
+      assert Sleep.list_day_sessions(~D[2022-05-17]) == [sleep3, sleep2]
+    end
+
+    test "create_session/1 with valid data creates a session" do
+      assert {:ok, sleep} =
+               Sleep.create_session(%{
+                 started_at: ~N[2022-05-17 18:30:00],
+                 ended_at: ~N[2022-05-18 06:00:00]
+               })
+    end
+
+    test "create_session/1 with invalid data returns error changeset" do
+      sleep_fixtures()
+      # Missing attrs
+      assert {:error, %Ecto.Changeset{}} = Sleep.create_session(%{})
+
+      # Start or end not in the past
+      now = NaiveDateTime.local_now()
+      errors = refute_session(now, NaiveDateTime.add(now, 3600))
+      refute :started_at in errors
+      assert "must be in the past" in errors.ended_at
+
+      # Start and end inverted
+      errors = refute_session(~N[2022-05-17 09:00:00], ~N[2022-05-17 08:00:00])
+      assert "end must be after start" in errors.duration
+
+      # Sleep time too short
+      errors = refute_session(~N[2022-05-17 09:00:00], ~N[2022-05-17 09:02:00])
+      assert "must be at least 5 minutes" in errors.duration
+
+      # Started before existing sleep ended
+      errors = refute_session(~N[2022-05-17 10:00:00], ~N[2022-05-17 11:00:00])
+      assert "overlaps with existing session" in errors.started_at
+
+      # Ended after existing sleep started
+      errors = refute_session(~N[2022-05-17 12:00:00], ~N[2022-05-17 14:00:00])
+      assert "overlaps with existing session" in errors.ended_at
+    end
+
+    defp refute_session(started_at, ended_at) do
+      result = Sleep.create_session(%{started_at: started_at, ended_at: ended_at})
+      assert {:error, %Ecto.Changeset{} = changeset} = result
+      errors_on(changeset)
+    end
+
+    test "delete_session/1 deletes the session" do
+      [sleep | _] = sleep_fixtures()
+      assert {:ok, sleep} = Sleep.delete_session(sleep)
+      refute sleep in Sleep.list_day_sessions(sleep.started_at)
+    end
+  end
+end
